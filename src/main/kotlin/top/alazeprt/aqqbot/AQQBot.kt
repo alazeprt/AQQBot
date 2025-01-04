5137539d01a7929075b51c362205115cae778501
alazeprt <nono135246@126.com>
2025-01-04 11:54:39 +0800

    Fix bugs on issue #16

--- src/main/kotlin/top/alazeprt/aqqbot/AQQBot.kt
+++ src/main/kotlin/top/alazeprt/aqqbot/AQQBot.kt
@@ -52,7 +52,7 @@ object AQQBot : Plugin() {
         } catch (e: ClassNotFoundException) {
             isBukkit = false
         }
-        val metrics = Metrics(24071, "1.0.11", Platform.CURRENT)
+        val metrics = Metrics(24071, "1.0.12", Platform.CURRENT)
         info("Loading data...")
         val configFile = releaseResourceFile("config.yml", replace = false)
         config = Configuration.loadFromFile(configFile)
