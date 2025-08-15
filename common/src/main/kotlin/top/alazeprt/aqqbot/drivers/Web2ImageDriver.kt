0e254b9a85b6c5ee7273246f8d20736a9dcd37ab
alazeprt <nono135246@126.com>
2025-08-15 21:18:23 +0800

    fix: html2image feature cannot download browser automatically

--- common/src/main/kotlin/top/alazeprt/aqqbot/drivers/Web2ImageDriver.kt
+++ common/src/main/kotlin/top/alazeprt/aqqbot/drivers/Web2ImageDriver.kt
@@ -5,6 +5,7 @@ import com.microsoft.playwright.Browser
 import com.microsoft.playwright.BrowserType
 import com.microsoft.playwright.Page
 import com.microsoft.playwright.Playwright
+import com.microsoft.playwright.impl.driver.Driver
 import com.microsoft.playwright.options.LoadState
 import com.microsoft.playwright.options.ScreenshotType
 import top.alazeprt.aqqbot.AQQBot
@@ -13,6 +14,7 @@ import top.alazeprt.aqqbot.util.LogLevel
 import java.io.File
 import java.nio.file.Files
 import java.util.*
+import java.util.concurrent.TimeUnit
 
 
 class Web2ImageDriver(val plugin: AQQBot) {
@@ -40,6 +42,31 @@ class Web2ImageDriver(val plugin: AQQBot) {
             val decompressedDriverFolder = tmpFolder.resolve("driver").resolve(platformDir())
             decompressedDriverFolder.toFile().copyRecursively(driverFolder)
             plugin.log(LogLevel.INFO, "Decompressed drivers to: ${driverFolder.absolutePath}")
+        }
+        if (driverFolder.resolve("node").isFile && !driverFolder.resolve("node").canExecute()) {
+            driverFolder.resolve("node").setExecutable(true)
+        }
+        if (driverFolder.resolve("node.exe").isFile && !driverFolder.resolve("node.exe").canExecute()) {
+            driverFolder.resolve("node.exe").setExecutable(true)
+        }
+        plugin.log(LogLevel.INFO, "Downloading the webkit ...")
+        try {
+            val pb: ProcessBuilder = createProcessBuilder()
+            pb.command().add("install")
+            pb.redirectError(ProcessBuilder.Redirect.INHERIT)
+            pb.redirectOutput(ProcessBuilder.Redirect.INHERIT)
+            val p = pb.start()
+            val result = p.waitFor(10, TimeUnit.MINUTES)
+            if (!result) {
+                p.destroy()
+                throw java.lang.RuntimeException("Timed out waiting for browsers to install")
+            }
+            if (p.exitValue() != 0) {
+                throw java.lang.RuntimeException("Failed to install browsers, exit code: " + p.exitValue())
+            }
+        } catch (e: Exception) {
+            plugin.log(LogLevel.ERROR, "Failed to install browsers: ${e.message}")
+            return
         }
         plugin.log(LogLevel.INFO, "Testing the driver ...")
         updateProperty()
@@ -99,5 +126,33 @@ class Web2ImageDriver(val plugin: AQQBot) {
             plugin.log(LogLevel.ERROR, "Failed to convert web page to image: ${e.message}")
         }
         return null
+    }
+
+    fun createProcessBuilder(): ProcessBuilder {
+        val node = if (System.getProperty("os.name").lowercase(Locale.getDefault())
+                .contains("windows")
+        ) "node.exe" else "node"
+        val nodePath = driverFolder.resolve(node).absolutePath
+        val pb = ProcessBuilder(nodePath)
+        pb.command().add(driverFolder.resolve("package").resolve("cli.js").absolutePath)
+        pb.environment().put("PW_LANG_NAME", "java")
+        pb.environment().put("PW_LANG_NAME_VERSION", getMajorJavaVersion())
+        val version = Driver::class.java.getPackage().implementationVersion
+        if (version != null) {
+            pb.environment().put("PW_CLI_DISPLAY_VERSION", version)
+        }
+        return pb
+    }
+
+    private fun getMajorJavaVersion(): String {
+        val version = System.getProperty("java.version")
+        if (version.startsWith("1.")) {
+            return version.substring(2, 3)
+        }
+        val dot = version.indexOf(".")
+        if (dot != -1) {
+            return version.substring(0, dot)
+        }
+        return version
     }
 }
