e96f9b330bcf36c2ae492ba6ecb12d85bad85e20
alazeprt <nono135246@126.com>
2025-08-20 13:46:42 +0800

    refactor: update upstream (aonebot)

--- common/src/main/kotlin/top/alazeprt/aqqbot/AQQBot.kt
+++ common/src/main/kotlin/top/alazeprt/aqqbot/AQQBot.kt
@@ -229,7 +229,7 @@ interface AQQBot: ConfigProvider, CommandProvider, DataProvider, HookProvider, T
         val aonebotLib = Library.builder()
             .groupId("com{}github{}alazeprt")
             .artifactId("AOneBot")
-            .version("1.0.16-beta")
+            .version("1.0.18-beta")
             .relocate("com{}google{}code{}gson", "top{}alazeprt{}aonebot{}lib{}com{}google")
             .resolveTransitiveDependencies(true)
             .build()
